652ad62592a00a33b2b18c413bb0fe5e4a02a2fa
Brunoo652 <breboredoa@fpcoruna.afundacion.org>
2023-01-08 14:44:01 +0100

    Repositorio creado

---
+++ API-REST/JSON.py
@@ -0,0 +1,72 @@
+[
+    {
+        "titulo": "El señor de los anillos: La comunidad del anillo",
+        "url_caratula": "https://m.media-amazon.com/images/M/MV5BN2EyZjM3NzUtNWUzMi00MTgxLWI0NTctMzY4M2VlOTdjZWRiXkEyXkFqcGdeQXVyNDUzOTQ5MjY@._V1_.jpg",
+        "autor": "J.R.R. Tolkien",
+        "actores": ["Elijah Wood", "Ian McKellen", "Orlando Bloom"],
+        "genero": "Aventura, Fantasía"
+    },
+    {
+        "titulo": "Star Wars: Episodio VII - El despertar de la fuerza",
+        "url_caratula": "https://m.media-amazon.com/images/M/MV5BOTAzODEzNDAzMl5BMl5BanBnXkFtZTgwMDU1MTgzNzE@._V1_.jpg",
+        "autor": "George Lucas",
+        "actores": ["Harrison Ford", "Mark Hamill", "Carrie Fisher"],
+        "genero": "Ciencia ficción, Aventura"
+    },
+    {
+        "titulo": "Harry Potter y la piedra filosofal",
+        "url_caratula": "https://m.media-amazon.com/images/M/MV5BNjQ3NWNlNmQtMTE5ZS00MDdmLTlkZjUtZTBlM2UxMGFiMTU3XkEyXkFqcGdeQXVyNjUwNzk3NDc@._V1_.jpg",
+        "autor": "J.K. Rowling",
+        "actores": ["Daniel Radcliffe", "Emma Watson", "Rupert Grint"],
+        "genero": "Fantasía, Aventura"
+    },
+    {
+        "titulo": "Interstellar",
+        "url_caratula": "https://m.media-amazon.com/images/M/MV5BZjdkOTU3MDktN2IxOS00OGEyLWFmMjktY2FiMmZkNWIyODZiXkEyXkFqcGdeQXVyMTMxODk2OTU@._V1_.jpg",
+        "autor": "Christopher Nolan",
+        "actores": ["Matthew McConaughey", "Anne Hathaway", "Jessica Chastain"],
+        "genero": "Ciencia ficción, Drama"
+    },
+    {
+        "titulo": "Moonlight",
+        "url_caratula": "https://m.media-amazon.com/images/M/MV5BNzQxNTIyODAxMV5BMl5BanBnXkFtZTgwNzQyMjQyMjI@._V1_.jpg",
+        "autor": "Barry Jenkins",
+        "actores": ["Mahershala Ali", "Alex Hibbert", "Rohan Chand"],
+        "genero": "Drama"
+    },
+    {
+        "titulo": "Mad Max: Furia en el camino",
+        "url_caratula": "https://m.media-amazon.com/images/M/MV5BOTUwODM5MTctMzgxNi00MzlkLTg2YTgtN2IyMzUxYzMzNjUxXkEyXkFqcGdeQXVyNTUyMzE4Mzg@._V1_.jpg",
+        "autor": "George Miller",
+        "actores": ["Tom Hardy", "Charlize Theron", "Nicholas Hoult"],
+        "genero": "Acción, Aventura"
+    },
+    {
+        "titulo": "El renacido",
+        "url_caratula": "https://m.media-amazon.com/images/M/MV5BMTcxMjQ1NTEwMl5BMl5BanBnXkFtZTgwMDI1OTA1NzE@._V1_.jpg",
+        "autor": "Alejandro G. Iñárritu",
+        "actores": ["Leonardo DiCaprio", "Tom Hardy", "Will Poulter"],
+        "genero": "Aventura, Drama"
+    },
+    {
+        "titulo": "La forma del agua",
+        "url_caratula": "https://m.media-amazon.com/images/M/MV5BNGNiNWQ5M2MtNGI0OC00MjM1LWJkZTItZTJkMjM3NjM0NTEzXkEyXkFqcGdeQXVyMjM4NTM5NDY@._V1_.jpg",
+        "autor": "Guillermo del Toro",
+        "actores": ["Sally Hawkins", "Octavia Spencer", "Michael Shannon"],
+        "genero": "Fantasía, Drama"
+    },
+    {
+        "titulo": "La llegada",
+        "url_caratula": "https://m.media-amazon.com/images/M/MV5BMzUzNDM2NzM2MV5BMl5BanBnXkFtZTgwNTM3NTg4OTE@._V1",
+        "autor": "Denis Villeneuve",
+        "actores": ["Amy Adams", "Jeremy Renner", "Forest Whitaker"],
+        "genero": "Ciencia ficción, Drama"
+    },
+    {
+        "titulo": "La la land",
+        "url_caratula": "https://m.media-amazon.com/images/M/MV5BMTExMzU0ODcxNDheQTJeQWpwZ15BbWU4MDE1OTI4MzAy._V1_.jpg",
+        "autor": "Damien Chazelle",
+        "actores": ["Emma Stone", "Ryan Gosling", "Rosemarie DeWitt"],
+        "genero": "Musical, Romantica"
+    }
+]
